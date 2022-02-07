ff98ab5f457f872d1ccf4f1c0c5286b1ec8dc2b8
akmaralmazhitova <akmaral.mazhitova.kk@gmail.com>
2022-02-07 15:40:10 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Class cl = new Class(8, "January",new int[]{34, 56, 78});
+        cl.getInfoClass();
+
+    }
+}
